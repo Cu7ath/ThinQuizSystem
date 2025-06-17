15a4190ac86c25c3ff8d97e51f8861e4fe98ccfe
User <User@ASUS-Cu7>
2025-06-17 15:21:34 +0800

    update database

--- src/main/java/java/util/DBConnection.java
+++ src/main/java/java/util/DBConnection.java
@@ -15,9 +15,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/thinquizsystem";
-    private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = ""; // atau password XAMPP anda
+    private static final String JDBC_URL=System.getenv("DB_URL");
+
+    private static final String JDBC_USERNAME = System.getenv("DB_USER");
+
+    private static final String JDBC_PASSWORD = System.getenv("DB_PASS");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);

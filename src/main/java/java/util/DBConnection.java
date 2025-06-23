052fa5fa6e2200801ed83d774fafbcbe1645f337
User <User@ASUS-Cu7>
2025-06-23 20:50:11 +0800

    update

--- src/main/java/java/util/DBConnection.java
+++ src/main/java/java/util/DBConnection.java
@@ -10,11 +10,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String JDBC_URL=System.getenv("JDBC_URL");
+    private static final String JDBC_URL=System.getenv("DB_URL");
 
-    private static final String JDBC_USERNAME = System.getenv("JDBC_USERNAME");
+    private static final String JDBC_USERNAME = System.getenv("DB_USERNAME");
 
-    private static final String JDBC_PASSWORD = System.getenv("JDBC_PASSWORD");
+    private static final String JDBC_PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         try {
